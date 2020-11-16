a84b832f82a534d81fd560ca128371e511862724
Vladimir Svishch <5693031@gmail.com>
2020-11-16 13:56:25 +0300

    4. *Добавить кнопку в разметку и повесить на нее clickListener в Activity. 5. Убедиться, что все работает.

--- app/src/main/java/ru/geekbrains/AndroidKotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/AndroidKotlin/MainActivity.kt
@@ -1,11 +1,28 @@
 package ru.geekbrains.AndroidKotlin
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // OK
+        findViewById<Button>(R.id.button).setOnClickListener { findViewById<TextView>(R.id.text).setText("Ok") }
+        /*
+        findViewById(R.id.select_city_button_ok).setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnMain();
+
+            }
+        }));
+         */
     }
 }
